e5eba22ad2b8c564eab1b24674fafbd18a3e33a0
Joe Fabisevich <Joe@fabisevi.ch>
2017-09-04 10:32:12 -0400

    Updating tests to use explicit self

--- Tests/TypedNotificationTests.swift
+++ Tests/TypedNotificationTests.swift
@@ -15,39 +15,39 @@ final class TypedNotificationTests: XCTestCase {
     let passingTypedNotification = TestTypedNotification(payload: TypedNotificationTests.passingValue)
 
     lazy var passingNotification: Notification = {
-        return NotificationCenter.generateNotification(typedNotification: passingTypedNotification)
+        return NotificationCenter.generateNotification(typedNotification: self.passingTypedNotification)
     }()
 
     let failingTypedNotification = TestTypedNotification(payload: TypedNotificationTests.failingValue)
 
     lazy var failingNotification: Notification = {
-        return NotificationCenter.generateNotification(typedNotification: failingTypedNotification)
+        return NotificationCenter.generateNotification(typedNotification: self.failingTypedNotification)
     }()
     
     func testCorrectPayloadExtraction() {
         let payload = passingNotification.getPayload(notificationType: TestTypedNotification.self)
         let expectedPayload = TypedNotificationTests.passingValue
-        XCTAssertTrue(payload == expectedPayload, "passingTypedNotification was expected to carry a payload of true")
+        XCTAssertTrue(payload == expectedPayload, "passingNotification was expected to carry a payload of true")
     }
     
     func testIncorrectPayloadExtract() {
         let payload = failingNotification.getPayload(notificationType: TestTypedNotification.self)
         let expectedPayload = TypedNotificationTests.passingValue
-        XCTAssertFalse(payload == expectedPayload, "failingTypedNotification was expected to carry a payload of false")
+        XCTAssertFalse(payload == expectedPayload, "failingNotification was expected to carry a payload of false")
     }
 
     func testCorrectNotificationNameGeneration() {
         let generatedNotificationName = NotificationCenter.generateNotificationName(type: TestTypedNotification.self)
-        XCTAssertTrue(generatedNotificationName == passingNotification.name, "passingTypedNotification should have a name")
+        XCTAssertTrue(generatedNotificationName == passingNotification.name, "generatedNotificationName was expected to generate a name")
     }
 
     func testCorrectNotificationGeneration() {
-        let generatedNotification = NotificationCenter.generateNotification(typedNotification: passingTypedNotification)
-        XCTAssertTrue(generatedNotification == passingNotification, "passingTypedNotification should generate a matching notification")
+        let generatedNotification = NotificationCenter.generateNotification(typedNotification: self.passingTypedNotification)
+        XCTAssertTrue(generatedNotification == passingNotification, "passingTypedNotification was expected to generate a matching notification")
     }
 
     func testIncorrectNotificationGeneration() {
-        let generatedNotification = NotificationCenter.generateNotification(typedNotification: failingTypedNotification)
-        XCTAssertFalse(generatedNotification == passingNotification, "passingTypedNotification should generate a matching notification")
+        let generatedNotification = NotificationCenter.generateNotification(typedNotification: self.failingTypedNotification)
+        XCTAssertFalse(generatedNotification == passingNotification, "failingTypedNotification was expected to generate an incorrect notification")
     }
 }
